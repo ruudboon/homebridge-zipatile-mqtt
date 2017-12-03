8de9750bff8cae2ba1d1bb0e679af92d45806063
Ruud Boon <ruud@t-boonproductions.nl>
2017-12-03 21:15:04 +0100

    Fixed links

---
+++ README.md
@@ -0,0 +1,46 @@
+# homebridge-mqtt-temperature
+Get all zipatile data via MQTT in Homebridge
+
+Installation
+--------------------
+    sudo npm install -g homebridge-zipatile-mgtt
+
+
+Sample HomeBridge Configuration
+--------------------
+    {
+      "bridge": {
+        "name": "HomeBridge",
+        "username": "AB:DC:12:3A:22:CE",
+        "port": 51826,
+        "pin": "321-45-123"
+      },
+
+      "description": "",
+
+      "accessories": [
+        {
+          "accessory": "zipatile-mgtt",
+          "name": "Zipatile",
+          "serial": "ZT5SERIALNUMBER",
+          "url": "mqtt://localhost",
+          "username": "username",
+          "password": "password"
+        }
+      ],
+
+      "platforms": []
+    }
+
+
+---------------------
+
+`serial` set this to the serial number of your zipatile
+
+`username` and `password` are optional if you don't use MQTT authentication.
+
+
+#### Credits
+
+[homebridge-netatmo](https://github.com/planetk/homebridge-netatmo)
+[homebridge-mqtt-temperature](https://github.com/mcchots/homebridge-mqtt-temperature)
---
+++ index.js
@@ -0,0 +1,94 @@
+var Service, Characteristic;
+var mqtt = require('mqtt');
+
+module.exports = function(homebridge) {
+  Service = homebridge.hap.Service;
+  Characteristic = homebridge.hap.Characteristic;
+  connectMqtt();
+
+  homebridge.registerAccessory("homebridge-zipatile-mgtt", "zipatile-mgtt", AmbientLightLevel);
+}
+
+function AmbientLightLevel(log, config) {
+
+
+
+  this.log = log;
+  this.name = config["name"];
+  this.url = config['url'];
+  this.topic = 'zipato/zipatile/' + config['serial'] + '/allEvents';
+  this.client_Id 		= 'homebridg_zipatile_mqtt' + Math.random().toString(16).substr(2, 8);
+  this.options = {
+    keepalive: 10,
+    clientId: this.client_Id,
+    protocolId: 'MQTT',
+    protocolVersion: 4,
+    clean: true,
+    reconnectPeriod: 1000,
+    connectTimeout: 30 * 1000,
+    serialnumber: config["serial"] || this.client_Id,
+    max_temperature: config["maxTemperature"] || 100,
+    min_temperature: config["minTemperature"] || -50,
+    username: config["username"],
+    password: config["password"],
+    will: {
+      topic: 'WillMsg',
+      payload: 'Connection Closed abnormally..!',
+      qos: 0,
+      retain: false
+    },
+    rejectUnauthorized: false
+  };
+
+
+  this.service = new Service.LightSensor(this.name);
+  this.client  = mqtt.connect(this.url, this.options);
+
+
+  var that = this;
+  this.client.subscribe(this.topic);
+
+  this.client.on('message', function (topic, payload) {
+    // message is Buffer
+    data = JSON.parse(payload.toString());
+    if (data === null) {return null}
+    switch (data.function){
+      case 'LIGHT':
+        that.light = parseFloat(data.value);
+        that.service
+          .setCharacteristic(Characteristic.CurrentAmbientLightLevel, that.light);
+        break;
+    }
+    console.log("json", data);
+
+
+
+  });
+
+  this.client.on('connect', function(){
+    console.log('MQTT connected');
+  });
+
+
+
+  this.service
+    .getCharacteristic(Characteristic.CurrentAmbientLightLevel)
+    .on('get', this.getState.bind(this));
+
+  // this.service
+  //   .getCharacteristic(Characteristic.CurrentTemperature)
+  //   .setProps({minValue: this.options["min_temperature"]});
+  //
+  // this.service
+  //   .getCharacteristic(Characteristic.CurrentTemperature)
+  //   .setProps({maxValue: this.options["max_temperature"]});
+
+}
+
+AmbientLightLevel.prototype.getState = function(callback) {
+  callback(null, this.light);
+}
+
+AmbientLightLevel.prototype.getServices = function() {
+  return [this.service];
+}
